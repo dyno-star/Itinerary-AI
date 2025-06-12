e8954318af83bb42f8cc3233d9f30d81bab7f99d
dyno-star <da893@drexel.edu>
2025-06-12 12:49:18 -0400

    added keys

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import requests
 import json
 from openai import OpenAI
 import datetime
+import os
+from dotenv import load_dotenv
 
-OPENWEATHERMAP_API_KEY = ""
-GOOGLE_PLACES = ""
-api_key = ""
+load_dotenv()
+
+weather_api_key = os.getenv("OPENWEATHERMAP_API_KEY")
+google_api_key = os.getenv("GOOGLE_PLACES")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+print(weather_api_key, google_api_key, openai_api_key)
+
